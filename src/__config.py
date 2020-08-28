e9af8848d6f1f998c2ee5d00405991ae6af8facf
lablnet <mumerfarooqlablnet01@gmail.com>
2020-08-28 10:36:51 +0500

    adding files

---
+++ src/__config.py
@@ -0,0 +1,32 @@
+"""__config.py: Config manipulation."""
+__author__ = "Muhammad Umer Farooq"
+__license__ = "GPL-3"
+__version__ = "1.0.0"
+__maintainer__ = "Muhammad Umer Farooq"
+__email__ = "contact@muhammadumerfarooq.me"
+__status__ = "Production"
+
+import json
+import os
+
+
+def get_config(config, root=""):
+    """
+     Get config value from config file.
+     Args:
+         config: key the value you want to get!.
+     Returns:
+         mixed.
+     Raises:
+         None.
+     """
+
+    if os.path.exists(root+"/config.json"):
+        file = open(root+"/config.json", "r")
+        data = file.read()
+        configuration = json.loads(data)
+        if config in configuration:
+            return configuration[config]
+        else:
+            return None
+    return None
---
+++ src/_sqlite.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+# creating database
+def create_db():
+    pass
+
+
+def create_tables():
+    pass
+
+
+def insert():
+    pass
+
+
+def update():
+    pass
