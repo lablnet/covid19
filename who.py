a869ba4afa24ba1598b176ac67a5d93604397b02
Usman-Naeem <usman.naeem2212@gmail.com>
2020-09-07 15:16:19 +0500

    Adding configuration and parser

---
+++ who.py
@@ -0,0 +1,13 @@
+from src._sqlite import _sqlite
+from who_config import get_config
+import json
+import csv
+
+s = _sqlite()
+conn = s.conn(get_config("database", './'))
+
+
+def csv_to_list() :
+    with open('WHO-COVID-19-global-data.csv', newline='') as csvfile:
+        c_reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        return list(c_reader)
