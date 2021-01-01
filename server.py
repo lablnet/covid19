5467a8ca9861b951cd2c7870ee87739b2d314eb8
lablnet <mumerfarooqlablnet01@gmail.com>
2021-01-01 08:28:51 +0500

    refactor

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import request
 from src._sqlite import _sqlite
 from src.__config import get_config
 import json
 app = Flask(__name__)
+
+
+# Prefix for all routes.
+app.config["APPLICATION_ROOT"] = "/api/"
 
 
 @app.route('/')
@@ -11,10 +16,36 @@ def index():
     s = _sqlite
     conn = s.conn(get_config("database", './'))
     data = conn.get_data("cases", None, None)  # TableName, From(date), To(date)
-    # data = conn.get_data("cases", '2020-12-01', '2020-12-31')
     conn.close()
 
     return json.dumps(data)
+
+
+@app.route('/feed')
+def feed():
+    page = request.args['page']
+
+
+@app.route('/summery')
+def summery():
+    to = request.args['to']
+    frm = request.args['from']
+
+    s = _sqlite
+    conn = s.conn(get_config("database", './'))
+    data = conn.get_data("cases", frm, to)  # TableName, From(date), To(date)
+    conn.close()
+
+    return json.dumps(data)
+
+@app.route('/newsletter')
+def newsletter():
+    data = request.form.to_dict()
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.form.to_dict()
 
 
 app.run()
