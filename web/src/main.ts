fa57c021d706e7154468dad42679c5ab73758a44
lablnet <mumerfarooqlablnet01@gmail.com>
2021-01-01 07:06:24 +0500

    intregrate bootstrap 5

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,5 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap'
+
+import './assets/scss/app.scss'
 
 createApp(App).use(store).use(router).mount('#app')
