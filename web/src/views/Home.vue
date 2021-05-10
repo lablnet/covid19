10479a8429732a1eb55d6e5a76feececc330f354
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-05-10 14:59:17 +0500

    fix ui issue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -56,10 +56,10 @@
             <div class="card">
                 <div class="card-header">
                     <div>
-                        <div class="" style="float: left">
+                        <div style="float: left">
                           <h3 class="subtitle feed-title">Cases Confirmations</h3>
                         </div>
-                        <div class="col " style="float:right">
+                        <div style="float:right">
                             <button class="feedbutton" v-if="page >= 2" @click="prevPage">
                               <svg viewBox="64 64 896 896" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path></svg>
                             </button>
