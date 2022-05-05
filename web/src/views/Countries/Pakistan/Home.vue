f50982e5f58776ca6f9fd8a2e618c0e2d726631b
Muhammad Umer Farooq <umer@lablnet.com>
2022-05-05 10:14:11 +0500

    add banner as the government site is down

--- web/src/views/Countries/Pakistan/Home.vue
+++ web/src/views/Countries/Pakistan/Home.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="container">
+    <div class="mt-3 alert alert-danger alert-dismissible">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+
+      <p class="content black">
+        The Government of Pakistan's data source site has been down for a few
+        days now so we are unable to retrieve data automatically, even manually.
+        The data will be updated when the site is reactivated by the Government
+        of Pakistan. We want Pakistan to focus on its IT as well.
+        <br />
+        <a href="https://covid.gov.pk/stats/pakistan" target="_blank"
+          >Click here to view the official data source site.</a>
+      </p>
+    </div>
     <div class="row">
       <div class="col-md-8">
         <h1 class="title mt-3"><b>COVID-19 SITUATION IN PAKISTAN</b></h1>
@@ -11,19 +24,23 @@
     <h2 class="subtitle mt-1">COVID-19 SITUATION IN PAKISTAN</h2>
     <p class="content mt-3 mb-5">
       The coronavirus aka COVID-19, is igniting fear worldwide because of number
-      of reasons. there are vaccines available for
-      COVID-19. Alongside developing vaccine for COVID-19, researchers are also
-      trying to predict its behaviour.
+      of reasons. there are vaccines available for COVID-19. Alongside
+      developing vaccine for COVID-19, researchers are also trying to predict
+      its behaviour.
     </p>
 
-    <h2 class="subtitle mt-2 mb-2"><b>Vaccine Statistic</b> <span class="badge badge-danger">new</span></h2>
+    <h2 class="subtitle mt-2 mb-2">
+      <b>Vaccine Statistic</b> <span class="badge badge-danger">new</span>
+    </h2>
     <div class="row">
       <div class="col-md-4">
         <div class="card">
           <div class="container">
             <h1 class="title twelve mt-2"><b>Partially Vaccinated</b></h1>
-            <p class="stat mx-2 mt-2 text-info"><b>{{ vaccine.total_partially }}</b> <sub
-              v-if="isToday()">+{{ vaccine.last_partially }}</sub></p>
+            <p class="stat mx-2 mt-2 text-info">
+              <b>{{ vaccine.total_partially }}</b>
+              <sub v-if="isToday()">+{{ vaccine.last_partially }}</sub>
+            </p>
           </div>
         </div>
       </div>
@@ -31,8 +48,10 @@
         <div class="card">
           <div class="container">
             <h1 class="title twelve mt-2"><b>Fully Vaccinated</b></h1>
-            <p class="stat mx-2 mt-2 text-success"><b>{{ vaccine.total_fully }}</b> <sub
-              v-if="isToday()">+{{ vaccine.last_fully }}</sub></p>
+            <p class="stat mx-2 mt-2 text-success">
+              <b>{{ vaccine.total_fully }}</b>
+              <sub v-if="isToday()">+{{ vaccine.last_fully }}</sub>
+            </p>
           </div>
         </div>
       </div>
@@ -40,8 +59,10 @@
         <div class="card">
           <div class="container">
             <h1 class="title twelve mt-2"><b>Total Doses Administered</b></h1>
-            <p class="stat mx-2 mt-2 text-primary"><b>{{ vaccine.total_doses }}</b> <sub
-              v-if="isToday()">+{{ vaccine.last_doses }}</sub></p>
+            <p class="stat mx-2 mt-2 text-primary">
+              <b>{{ vaccine.total_doses }}</b>
+              <sub v-if="isToday()">+{{ vaccine.last_doses }}</sub>
+            </p>
           </div>
         </div>
       </div>
@@ -53,8 +74,10 @@
         <div class="card">
           <div class="container">
             <h1 class="title twelve mt-2"><b>Total tests</b></h1>
-            <p class="stat mx-2 mt-2 text-info"><b>{{ summery.total_tests }}</b> <sub
-              v-if="isToday()">+{{ summery.last_tests }}</sub></p>
+            <p class="stat mx-2 mt-2 text-info">
+              <b>{{ summery.total_tests }}</b>
+              <sub v-if="isToday()">+{{ summery.last_tests }}</sub>
+            </p>
           </div>
         </div>
       </div>
@@ -62,8 +85,10 @@
         <div class="card">
           <div class="container">
             <h1 class="title twelve mt-2"><b>Total Cases</b></h1>
-            <p class="stat mx-2 mt-2 text-warning"><b>{{ summery.total_cases }}</b> <sub
-              v-if="isToday()">+{{ summery.last_cases }}</sub></p>
+            <p class="stat mx-2 mt-2 text-warning">
+              <b>{{ summery.total_cases }}</b>
+              <sub v-if="isToday()">+{{ summery.last_cases }}</sub>
+            </p>
           </div>
         </div>
       </div>
@@ -71,8 +96,10 @@
         <div class="card">
           <div class="container">
             <h1 class="title twelve mt-2"><b>Recoveries</b></h1>
-            <p class="stat mx-2 mt-2 text-success"><b>{{ summery.total_recovered }}</b> <sub
-              v-if="isToday()">+{{ summery.last_recovered }}</sub></p>
+            <p class="stat mx-2 mt-2 text-success">
+              <b>{{ summery.total_recovered }}</b>
+              <sub v-if="isToday()">+{{ summery.last_recovered }}</sub>
+            </p>
           </div>
         </div>
       </div>
@@ -80,8 +107,10 @@
         <div class="card">
           <div class="container">
             <h1 class="title twelve mt-2"><b>Deceased</b></h1>
-            <p class="stat mx-2 mt-2 text-danger"><b>{{ summery.total_deaths }}</b> <sub
-              v-if="isToday()">+{{ summery.last_deaths }}</sub></p>
+            <p class="stat mx-2 mt-2 text-danger">
+              <b>{{ summery.total_deaths }}</b>
+              <sub v-if="isToday()">+{{ summery.last_deaths }}</sub>
+            </p>
           </div>
         </div>
       </div>
@@ -93,21 +122,37 @@
           <div class="card-header">
             <div>
               <div style="float: left">
-                <h1 class="subtitle ">Cases Confirmations</h1>
+                <h1 class="subtitle">Cases Confirmations</h1>
               </div>
-              <div style="float:right">
+              <div style="float: right">
                 <button class="feedbutton" v-if="page >= 2" @click="prevPage">
-                  <svg viewBox="64 64 896 896" width="0.5em" height="1em" fill="currentColor" aria-hidden="true"
-                       focusable="false" class="">
+                  <svg
+                    viewBox="64 64 896 896"
+                    width="0.5em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    focusable="false"
+                    class=""
+                  >
                     <path
-                      d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path>
+                      d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"
+                    ></path>
                   </svg>
                 </button>
                 <button class="feedbutton" @click="nextPage">
-                  <svg viewBox="64 64 896 896" width="0.5em" height="1em" fill="currentColor" aria-hidden="true"
-                       focusable="false" class="">
+                  <svg
+                    viewBox="64 64 896 896"
+                    width="0.5em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    focusable="false"
+                    class=""
+                  >
                     <path
-                      d="M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4z"></path>
+                      d="M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4z"
+                    ></path>
                   </svg>
                 </button>
               </div>
@@ -118,22 +163,29 @@
               <li v-for="(item, idx) in feed" :key="idx" style="display: block">
                 <p :class="getClass(item.type)" style="display: inline">
                   {{ item.desc }}
-
                 </p>
                 <p class="content" style="font-size: 11px">
                   {{ item.date }} &nbsp; &nbsp;
                   <a v-if="item.ref" :href="item.ref" target="_blank">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                         class="bi bi-link-45deg" viewBox="0 0 16 16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-link-45deg"
+                      viewBox="0 0 16 16"
+                    >
                       <path
-                        d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
+                        d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"
+                      />
                       <path
-                        d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"/>
+                        d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"
+                      />
                     </svg>
                   </a>
                 </p>
               </li>
-              <br/>
+              <br />
             </ol>
           </div>
         </div>
@@ -147,36 +199,53 @@
             <div class="table-responsive">
               <table class="table table-strip">
                 <thead class="subtitle">
-                <tr>
-                  <th>
-                    <span class="text-info">Name</span>
-                  </th>
-                  <th>
-                    <span class="text-warning">Infected</span>
-                  </th>
-                  <th>
-                    <span class="text-success">Recovered</span>
-                  </th>
-                  <th>
-                    <span class="text-danger">Deceased</span>
-                  </th>
-                  <th>
-                    <span class="text-warning">Active</span>
-                  </th>
-                </tr>
-
+                  <tr>
+                    <th>
+                      <span class="text-info">Name</span>
+                    </th>
+                    <th>
+                      <span class="text-warning">Infected</span>
+                    </th>
+                    <th>
+                      <span class="text-success">Recovered</span>
+                    </th>
+                    <th>
+                      <span class="text-danger">Deceased</span>
+                    </th>
+                    <th>
+                      <span class="text-warning">Active</span>
+                    </th>
+                  </tr>
                 </thead>
                 <tbody class="content">
-                <span style="display: none">{{ i = 0 }}</span>
-                <tr v-for="item in provience" :key="item">
-                  <td><b>{{ provienceName[i] }}</b></td>
-                  <td>{{ formatted(item.infected.total) }} <sub v-if="isToday() && item.infected.last > 0">+{{ item.infected.last }}</sub></td>
-                  <td>{{ formatted(item.recovered.total) }} <sub v-if="isToday() && item.recovered.last > 0">+{{ item.recovered.last }}</sub></td>
-                  <td>{{ formatted(item.deceased.total) }} <sub v-if="isToday() && item.deceased.last > 0">+{{ item.deceased.last }}</sub></td>
-                  <td v-if="this.getActive(item)">{{ this.getActive(item) }}</td>
-                  <span style="display: none">{{ i++ }}</span>
-                </tr>
-
+                  <span style="display: none">{{ (i = 0) }}</span>
+                  <tr v-for="item in provience" :key="item">
+                    <td>
+                      <b>{{ provienceName[i] }}</b>
+                    </td>
+                    <td>
+                      {{ formatted(item.infected.total) }}
+                      <sub v-if="isToday() && item.infected.last > 0"
+                        >+{{ item.infected.last }}</sub
+                      >
+                    </td>
+                    <td>
+                      {{ formatted(item.recovered.total) }}
+                      <sub v-if="isToday() && item.recovered.last > 0"
+                        >+{{ item.recovered.last }}</sub
+                      >
+                    </td>
+                    <td>
+                      {{ formatted(item.deceased.total) }}
+                      <sub v-if="isToday() && item.deceased.last > 0"
+                        >+{{ item.deceased.last }}</sub
+                      >
+                    </td>
+                    <td v-if="this.getActive(item)">
+                      {{ this.getActive(item) }}
+                    </td>
+                    <span style="display: none">{{ i++ }}</span>
+                  </tr>
                 </tbody>
               </table>
             </div>
@@ -212,7 +281,7 @@
       <div class="col-md-12">
         <div class="card">
           <div class="card-header">
-            <h1 class="subtitle">Daily Trend </h1>
+            <h1 class="subtitle">Daily Trend</h1>
           </div>
           <div class="container">
             <div id="myChart" style="width: 100%"></div>
@@ -220,7 +289,6 @@
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
