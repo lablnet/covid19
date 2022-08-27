e81b65365e27840abd1a25ac8c77f94987618468
Muhammad Umer Farooq <umer@lablnet.com>
2022-08-27 14:09:43 +0500

    Update About.vue

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -44,7 +44,7 @@
           <li><a href="https://github.com/lablnet" target="_blank">Muhammad Umer Farooq</a> <span class="content-about">(Lead Developer)</span>
           </li>
           <li><a href="https://github.com/AmeerHamza0220" target="_blank">Muhammad Ameer Hamza</a> <span
-            class="content-about">(Python Developer)</span></li>
+            class="content-about">(Data Scientist)</span></li>
           <li><a href="https://github.com/Usman-Naeem" target="_blank">Muhammad Usman Naeem</a> <span
             class="content-about">(Python Developer)</span></li>
           <li><a href="https://github.com/Zain-ul-Abdin1417" target="_blank">Zain ul Abdin</a> <span
